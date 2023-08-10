005116e8b2baa125ec8a77b67cda414205e88e6c
HadiChaaban <chaabanh@umich.edu>
2023-08-09 21:32:24 -0400

    Chapter 6

---
+++ Chapter6/HelloObject.py
@@ -0,0 +1,53 @@
+import pygame
+from Object import *
+from Mesh3D import *
+from Cube import *
+from Transform import *
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+pygame.init()
+screen_width = 500
+screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF|OPENGL)
+pygame.display.set_caption("OpenGL in Python")
+done = False
+white = pygame.Color(255, 255, 255)
+
+glMatrixMode(GL_PROJECTION)
+gluPerspective(60, (screen_width / screen_height), 0.1, 100.0)
+glMatrixMode(GL_MODELVIEW)
+glTranslatef(0.0, 0.0, -3)
+glEnable(GL_DEPTH_TEST)
+glEnable(GL_LIGHTING)
+glLight(GL_LIGHT0, GL_POSITION, (5, 5, 5, 1))
+glLightfv(GL_LIGHT0, GL_DIFFUSE, (1, 1, 1, 1))
+glEnable(GL_LIGHT0)
+glMaterialfv(GL_FRONT, GL_DIFFUSE, (0, 1, 0, 1))
+
+objects = []
+
+cube = Object("Cube")
+cube.add_component(Transform((0, 0, -1)))
+cube.add_component(Cube(GL_POLYGON, "images/brickwall.tif"))
+objects.append(cube)
+cube2 = Object("Cube2")
+cube2.add_component(Transform((0, 1, 0)))
+cube2.add_component(Cube(GL_POLYGON, "images/gore.tif"))
+objects.append(cube2)
+
+clock = pygame.time.Clock()
+fps = 30
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glRotate(5, 5, 5, 1)
+    for object in objects:
+        object.update()
+    pygame.display.flip()
+    clock.tick(fps)
+pygame.quit()
